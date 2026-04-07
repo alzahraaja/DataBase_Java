8161b8327637f9aec51128c1b2f0e3e4c2fe215a
awew <alzahrapramana@gmail.com>
2026-04-07 18:21:15 +0700

    database

---
+++ src/main/java/javatutorial/Main.java
@@ -0,0 +1,12 @@
+package javatutorial;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new FormKaryawan().setVisible(true);
+       
+        });
+    }
+}
